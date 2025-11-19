36067ca6d048d3df18626cea39e7b4d1bedafd16
Lukhachdigital <huynhxuyensondirector@gmail.com>
2025-11-19 13:25:58 +0700

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,36 @@
+export const APP_LINKS = {
+    HOME: "https://lamyoutubeai.com",
+    FREE_RESOURCES: "https://lamyoutubeai.com/free",
+    API_KEY_TUTORIAL: "https://www.youtube.com/watch?v=1Pi-xnnFq70",
+    GEMINI_API_KEY_GET: "https://aistudio.google.com/app/apikey",
+    OPENAI_API_KEY_GET: "https://platform.openai.com/api-keys"
+};
+
+export const SOCIAL_LINKS = {
+    YOUTUBE: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    FACEBOOK: "https://www.facebook.com/huynhxuyenson",
+    TIKTOK: "https://tiktok.com/@lamyoutubeai",
+    ZALO: "https://zalo.me/g/yesdti650",
+};
+
+export const TUTORIAL_LINKS: Record<string, string> = {
+    dashboard: "https://www.youtube.com/playlist?list=PLlpNqaFh8WE4y1ULxKgfjDWyCBWx4-LhL",
+    prompt_json: "https://www.youtube.com/watch?v=Uql-HZz8UnM",
+    whisk_flow: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    my_channel: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    viet_kich_ban: "https://www.youtube.com/watch?v=uQi0RKvnzek",
+    ai_prompt_veo31: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    auto_prompt: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    audio_chopping: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    audio_to_prompt: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    audio_to_prompt_video: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    create_thumbnail: "https://www.youtube.com/watch?v=9d9c5Q1nID8",
+    tao_anh_trend: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    create_video: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    seo_youtube: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    youtube_external: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1",
+    app_affiliate: "https://youtu.be/N_UfSbpBAjs?si=Sjc7QUzZ9lds3ZKg",
+    content_podcast: "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1"
+};
+
+export const FALLBACK_TUTORIAL = "https://www.youtube.com/channel/UCwSbzgfgu1iMfOR__AB4QGQ?sub_confirmation=1";
